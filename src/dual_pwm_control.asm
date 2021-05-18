d87464a77550401c2daa64e331a2433ce151d2ea
Guenther Beulen <guenther.beulen@web.de>
2021-05-18 16:51:47 +0200

    Initial commit.

---
+++ src/dual_pwm_control.asm
@@ -0,0 +1,81 @@
+        ;;
+        ;; @file
+        ;;
+
+#define  __16F882
+#include <p16f882.inc>
+	__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
+	__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
+        
+#include "constants.inc"
+#include "registers.inc"      
+
+        org 0
+        goto      wwcw_control
+
+        org 4
+
+#include "isr_timer1.inc"
+#include "change_intensity.inc"	
+#include "load_data.inc"
+
+        ;; 
+        ; @brief This program controls warm white and cold white led stripes.
+        ;
+        ; Timer 2 is responsible for the output of PWM signals on RC1 and RC2.
+        ; Changing of the duty cycles is done with Timer 1.
+        ;; 
+wwcw_control:
+        bcf       STATUS,RP0          ; Ensure ISR executes in Register Bank 0
+        bcf       STATUS,RP1
+
+        ;; Configure and Start timer 2
+        bsf     STATUS, RP0
+        movlw   0xFF
+        movwf   PR2
+        bcf     PIE1, TMR2IE
+        bcf     STATUS, RP0
+        bsf     T2CON, TMR2ON
+
+        ;; Configure and start timer 1
+        bsf     T1CON, T1CKPS1
+        bsf     T1CON, T1CKPS0
+        bsf     STATUS, RP0
+        bsf     PIE1, TMR1IE
+        bcf     STATUS, RP0
+        bsf     T1CON, TMR1ON
+	
+        ;; enabling PWM mode
+	clrf	CCP1CON
+	bsf	CCP1CON,CCP1M3
+	bsf	CCP1CON,CCP1M2
+	clrf	CCP2CON
+	bsf	CCP2CON,CCP2M3
+	bsf	CCP2CON,CCP2M2
+
+	;; pin 1 and pin 2 are outputs
+	bsf	STATUS,RP0
+	bcf	TRISC, LED_WW
+	bcf	TRISC, LED_CW
+	bcf	STATUS,RP0
+       
+        ;; Clear Data
+        clrf    darker
+        movlw   LED_WW
+        movwf   darker
+        movlw   MAX
+        movwf   pointer_ww
+        movlw   0x00
+        clrf    pointer_cw        
+
+        ;; Enable global interrupts
+        bsf     INTCON, GIE
+        bsf     INTCON, PEIE
+
+mainloop:
+        goto    mainloop
+
+#include "intensities_high.inc"
+#include "intensities_low.inc"
+        
+        end
